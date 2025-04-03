8ecad75d080125719730eb9cb2979bfedb21a65c
Victor Kinyanjui Ngigi <victor.ngigi@outlook.com>
2025-04-03 12:38:44 +0300

    ??

--- index.js
+++ index.js
@@ -2,12 +2,10 @@
 const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
 const returnFirstTwoDrivers = function (drivers) {
   return drivers.slice(0, 2);
-  console.log(drivers.slice(0, 2));
 };
 
 const returnLastTwoDrivers = function (drivers) {
   return drivers.slice(-2);
-  console.log(drivers.slice(-2));
 };
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
